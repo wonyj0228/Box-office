e920d6a2d1b117a43066e35999bdcc3327047f58
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-04-05 00:31:39 +0900

    home page 완료
    - top10 데이터 app.js에서 불러오기로 변경
    - 포스터 조회 안되는 케이스 방지

--- src/App.js
+++ src/App.js
@@ -43,15 +43,22 @@ function App() {
       )
     ).json();
     const data = rowData.boxOfficeResult.dailyBoxOfficeList;
-    setMovieChart(
-      data.map((movie) => {
-        return {
-          rank: movie.rank,
-          movieNm: movie.movieNm,
-          openDt: movie.openDt,
-        };
-      })
-    );
+    return data.map((movie) => {
+      return {
+        rank: movie.rank,
+        movieNm: movie.movieNm,
+      };
+    });
+  };
+
+  const getMovieDetail = async (movieNm) => {
+    const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+    const data = await (
+      await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${movieNm}&language=ko-KR`
+      )
+    ).json();
+    return data;
   };
 
   const getGenreList = async () => {
@@ -65,7 +72,28 @@ function App() {
   };
 
   useEffect(() => {
-    getMovieChart();
+    getMovieChart().then((res) => {
+      const newArr = [];
+      for (let i = 0; i < res.length; i++) {
+        getMovieDetail(res[i].movieNm).then((res2) => {
+          const data = res2.results[0];
+          newArr.push({ ...res[i], ...data });
+
+          if (newArr.length === 10) {
+            newArr.sort((a, b) => {
+              if (parseInt(a.rank) < parseInt(b.rank)) {
+                return -1;
+              } else if (parseInt(a.rank) > parseInt(b.rank)) {
+                return 1;
+              } else {
+                return 0;
+              }
+            });
+            setMovieChart(newArr);
+          }
+        });
+      }
+    });
     getGenreList();
   }, []);
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,5 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import Slider from 'react-slick';
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
 
 import Header from '../components/Header';
 import TitleBox from '../components/TitleBox';
@@ -15,40 +13,10 @@ const Home = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const carousel = useRef();
 
-  const getMovieDetail = async (movieNm) => {
-    const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
-    const data = await (
-      await fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${movieNm}&language=ko-KR`
-      )
-    ).json();
-    return data;
-  };
-
   useEffect(() => {
     if (movieChart.length === 10) {
       const top3 = movieChart.slice(0, 3);
-
-      const newArr = [];
-      for (let i = 0; i < 3; i++) {
-        getMovieDetail(top3[i].movieNm).then((res) => {
-          const data = res.results[0];
-          newArr.push({ ...top3[i], ...data });
-
-          if (newArr.length === 3) {
-            newArr.sort((a, b) => {
-              if (a.rank < b.rank) {
-                return -1;
-              } else if (a.rank > b.rank) {
-                return 1;
-              } else {
-                return 0;
-              }
-            });
-            setNowHot(newArr);
-          }
-        });
-      }
+      setNowHot(top3);
     }
   }, [movieChart]);
 
@@ -80,6 +48,10 @@ const Home = () => {
     slidesToShow: 2,
     slidesToScroll: 2,
     variableWidth: true,
+    autoplay: true,
+    speed: 1500,
+    autoplaySpeed: 4000,
+    cssEase: 'ease-in-out',
   };
 
   return (
@@ -118,13 +90,17 @@ const Home = () => {
           <Slider {...settings}>
             {movieChart.map((movie) => {
               return (
-                <div>
+                <div key={`top10_${movie.id}`}>
                   <div className="top10_movie">
                     <div className="top10_rank">{movie.rank}</div>
-                    <img
-                      alt="poster"
-                      src="https://search.pstatic.net/common?quality=75&direct=true&src=https%3A%2F%2Fmovie-phinf.pstatic.net%2F20240207_151%2F1707288437263gBsdd_JPEG%2Fmovie_image.jpg"
-                    />
+                    {movie.poster_path ? (
+                      <img
+                        alt="poster"
+                        src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                      />
+                    ) : (
+                      <div className="top10_noPoster">{movie.movieNm}</div>
+                    )}
                   </div>
                 </div>
               );
