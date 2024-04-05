570cab3927635b8b32532200a2fd84988b49af5d
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-04-06 04:42:16 +0900

    genre ids 부분 로직수정 필요

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,29 +1,129 @@
 import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
+import Genre from '../components/Genre';
+import { useContext, useEffect, useState } from 'react';
+import { GenreListContext, MovieChartContext } from '../App';
+import Slider from 'react-slick';
 
 const Detail = () => {
   const { id } = useParams();
+  const movieData = useContext(MovieChartContext);
+  const genreList = useContext(GenreListContext);
 
+  const [movieDetail, setMovieDetail] = useState({});
+  const [images, setImages] = useState([]);
+
+  const getGenreText = (genre_id) => {
+    if (genreList.length > 0) {
+      const idx = genreList.findIndex(
+        ({ id, _ }) => String(id) === String(genre_id)
+      );
+      return genreList[idx].name;
+    } else {
+      console.log(
+        movieDetail.genres.filter((gen) => String(gen.id) === genre_id)
+      );
+      return movieDetail.genres.filter((gen) => String(gen.id) === genre_id)[0]
+        .name;
+    }
+  };
+
+  const getDetails = async (id) => {
+    const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+    const data = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=ko-KR`
+    );
+    const json = await data.json();
+    return json;
+  };
+
+  const getImages = async () => {
+    const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+    const data = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${API_KEY}`
+    );
+    const json = await data.json();
+    return json;
+  };
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    cssEase: 'ease-in-out',
+  };
+
+  useEffect(() => {
+    const filteredData = movieData.filter((movie) => String(movie.id) === id);
+    if (filteredData.length === 0) {
+      getDetails(id).then((res) => {
+        console.log(res);
+        setMovieDetail(res);
+      });
+    } else {
+      setMovieDetail(filteredData[0]);
+    }
+
+    getImages().then((res) => {
+      setImages(res.backdrops);
+    });
+  }, [id]);
+  debugger;
   return (
     <div className="Detail">
-      <Header />
-      <div className="detail_wrapper">
-        <img alt="poster" />
-        <div className="detail_right-box">
-          <div className="detail_info">
-            <div className="detail_info-title"></div>
-            <div className="detail_info-plus">
-              <div className="detail_info-plus_date"></div>
-              <div className="detail_info-plus_review"></div>
+      {Object.keys(movieDetail).length !== 0 && images.length >= 1 ? (
+        <>
+          <Header />
+          <div className="detail_wrapper">
+            <div className="detail_top_wrapper">
+              <img
+                className="detail_poster"
+                alt="poster"
+                src={`https://image.tmdb.org/t/p/original${movieDetail.poster_path}`}
+              />
+              <div className="detail_right-box">
+                <div className="detail_info">
+                  <div className="detail_info-title">{movieDetail.movieNm}</div>
+                  <div className="detail_info-plus">
+                    <div className="detail_info-plus_date">
+                      {`${movieDetail.release_date} 개봉`}
+                    </div>
+                    <div className="detail_info-plus_review">
+                      {movieDetail.vote_average}
+                    </div>
+                  </div>
+                  <div className="detail_info-genres">
+                    {movieDetail.genres_ids.map((genre) => (
+                      <Genre key={genre.id} text={getGenreText(genre.id)} />
+                    ))}
+                  </div>
+                </div>
+                <div className="slider-container detail_img-carousel">
+                  <Slider {...settings}>
+                    {images.map((image) => {
+                      return (
+                        <img
+                          className="detail_backdrops"
+                          alt="backdrop"
+                          src={`https://image.tmdb.org/t/p/original${image.file_path}`}
+                        />
+                      );
+                    })}
+                  </Slider>
+                </div>
+              </div>
             </div>
-            <div className="detail_info-genres"></div>
+            <div className="detail_summary">{movieDetail.overview}</div>
           </div>
-          <div className="detail_img-carousel"></div>
-        </div>
-      </div>
-      <div className="detail_summary"></div>
-
-      <div className="detail_casts"></div>
+          <div className="detail_casts"></div>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
